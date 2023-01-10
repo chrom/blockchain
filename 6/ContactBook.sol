f7ac36fd2c2bb602b58a08c22525cf982e3a02aa
Andrew <dr.chrom@gmail.com>
2023-01-10 22:32:38 +0200

    HW 6

---
+++ 6/ContactBook.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.17;
+
+// Практика:
+// Створити смарт-контракт, який дозволяє наступний функціонал:
+// 1. записувати ім'я та адресу його власника;
+// 2. дозволяти записувати адреси та імена (контакти) інших користувачів, а також їх порядкові номери;
+// 3. дозволяти знаходити раніше додані контакти за їх порядковим номером або адресою;
+// 3. контролювати, щоб ніхто крім власника не міг додавати нові контакти;
+// 4. випускати подію "NewContact" кожен раз, коли додається новий контакт.
+
+contract ContactBook {
+    string private _name;
+    address private _address;
+    address[] _bookArray;
+    mapping(address => string) public bookMap; 
+
+    constructor(string memory ownerName) {
+        _name = ownerName;
+        _address = msg.sender;
+    }
+
+    function addContact(string memory personName, address personAddress) public onlyOner {
+        require(msg.sender != personAddress, "Owner can't add him self.");
+        _bookArray.push(personAddress);
+        bookMap[personAddress] = personName;
+        new NewContact(personName, personAddress);
+    }
+    
+    function getContactAddressByIndex(uint256 index) public view returns(address){
+        return _bookArray[index];
+    }
+
+    function getContactNameByAddrees(address personAddress) public view returns(string memory){
+        return bookMap[personAddress];
+    }
+
+    
+    function getContactNameByindex(uint256 index) public view returns(string memory){
+        return getContactNameByAddrees(getContactAddressByIndex(index));
+    }
+
+    modifier onlyOner{
+        require(msg.sender == _address, "Add contact can only owner");
+        _;
+    }
+
+    event NewContact(string personName, address personAddress)
+}
